d99601ea2d72b3e97531eb46d9bc9402cee443dd
Brian Vu <84050828+bvu12@users.noreply.github.com>
2022-11-27 18:45:49 -0500

    refactor Lobby

--- components/Lobby/Lobby.tsx
+++ components/Lobby/Lobby.tsx
@@ -1,59 +1,108 @@
-// import { Modal, Button, Group, TextInput } from "@mantine/core";
-// import { useForm } from "@mantine/form";
+import { useState, useEffect, useContext } from "react";
+import { SocketContext } from "../SocketContext/socket";
+import { Modal, Button, Group, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 
-// import { Player } from "../../shared/types";
+import { Player } from "../../shared/types";
 
-// export const Lobby = ({
-//   room,
-//   timer,
-//   resumeTimer,
-//   pauseTimer,
-//   isTimerPaused,
-//   restartTimer,
-//   stopTimer,
-//   players,
-// }) => {
-//   return (
-//     <div>
-//       <h1>You have joined room: {room}</h1>
-//       <div> {timer.toPrecision(4)} </div>
-//       {isTimerPaused && <button onClick={resumeTimer}> Resume timer</button>}
-//       {!isTimerPaused && <button onClick={pauseTimer}> Pause timer</button>}
-//       <button onClick={restartTimer}> Restart timer</button>
-//       <button onClick={stopTimer}> Stop timer</button>
-//       {players.map((player: Player) => {
-//         return socket.id === player.playerSocketId ? (
-//           <div>
-//             <Modal
-//               opened={opened}
-//               onClose={() => setOpened(false)}
-//               title="Introduce yourself!"
-//             >
-//               <form
-//                 onSubmit={form.onSubmit((values) => {
-//                   socket.emit("set_username", values.username);
-//                   setOpened(false);
-//                 })}
-//               >
-//                 <TextInput
-//                   withAsterisk
-//                   label="Enter your username!"
-//                   placeholder="Username"
-//                   data-autoFocus
-//                   {...form.getInputProps("username")}
-//                 />
-//                 <Group position="right" mt="md">
-//                   <Button type="submit">Submit</Button>
-//                 </Group>
-//               </form>
-//             </Modal>
-//             <Button onClick={() => setOpened(true)}>Open Modal</Button>
-//             <h2 key="playerSocketId"> {player.username} </h2>
-//           </div>
-//         ) : (
-//           <div key="playerSocketId"> {player.username} </div>
-//         );
-//       })}
-//     </div>
-//   );
-// };
+interface LobbyProps {
+  roomName: string;
+  isRoomOwner: boolean;
+  players: Player[];
+}
+
+export const Lobby = ({ roomName, isRoomOwner, players }: LobbyProps) => {
+  const socket = useContext(SocketContext);
+
+  // Modal states
+  const [opened, setOpened] = useState(false);
+  const form = useForm({
+    initialValues: {
+      username: "",
+    },
+
+    // validate: {
+    //   email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    // },
+  });
+
+  // Timer
+  const [timer, setTimer] = useState(0);
+  const [isTimerPaused, setIsTimerPaused] = useState(false);
+
+  if (timer < 0) {
+    socket.emit("stop_timer");
+  }
+
+  // Socket responses
+  const pauseTimer = () => {
+    socket.emit("pause_timer");
+  };
+  const resumeTimer = () => {
+    socket.emit("resume_timer");
+  };
+  const restartTimer = () => {
+    socket.emit("restart_timer");
+  };
+  const stopTimer = () => {
+    if (isRoomOwner) {
+      socket.emit("stop_timer");
+    } else {
+      alert("You don't have permission to do that!");
+    }
+  };
+
+  useEffect(() => {
+    socket.on("timer-countdown", (timer: number) => {
+      setTimer(timer);
+    });
+
+    socket.on("is-timer-paused", (isPaused: boolean) => {
+      setIsTimerPaused(isPaused);
+    });
+  });
+
+  return (
+    <div>
+      <h1>You have joined room: {roomName}</h1>
+      <div> {timer.toPrecision(4)} </div>
+      {isTimerPaused && <button onClick={resumeTimer}> Resume timer</button>}
+      {!isTimerPaused && <button onClick={pauseTimer}> Pause timer</button>}
+      <button onClick={restartTimer}> Restart timer</button>
+      <button onClick={stopTimer}> Stop timer</button>
+      {players.map((player: Player) => {
+        return socket.id === player.playerSocketId ? (
+          <div>
+            <Modal
+              opened={opened}
+              onClose={() => setOpened(false)}
+              title="Introduce yourself!"
+            >
+              <form
+                onSubmit={form.onSubmit((values) => {
+                  socket.emit("set_username", values.username);
+                  setOpened(false);
+                })}
+              >
+                <TextInput
+                  withAsterisk
+                  label="Enter your username!"
+                  placeholder="Username"
+                  data-autoFocus
+                  {...form.getInputProps("username")}
+                />
+                <Group position="right" mt="md">
+                  <Button type="submit">Submit</Button>
+                </Group>
+              </form>
+            </Modal>
+            <Button onClick={() => setOpened(true)}>Open Modal</Button>
+            <h2 key="playerSocketId"> {player.username} </h2>
+          </div>
+        ) : (
+          <div key="playerSocketId"> {player.username} </div>
+        );
+      })}
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { SocketContext } from "../components/SocketContext/socket";
 
 import { Player } from "../shared/types";
 import { LandingPage } from "../components/LandingPage/LandingPage";
-// import { Lobby } from "../components/Lobby/Lobby";
+import { Lobby } from "../components/Lobby/Lobby";
 
 function getRoomIdFromString(message: string): string {
   let roomId = message.match(/'(.*?)'/g);
@@ -22,67 +22,22 @@ export default function Home() {
   const socket = useContext(SocketContext);
 
   //Room State
-  const [room, setRoom] = useState<string>("");
+  const [roomName, setRoomName] = useState<string>("");
   const [isInRoom, setIsInRoom] = useState<boolean>(false);
-  const [isRoomOwner, setIsRoomOwner] = useState<boolean>(false);
   const [players, setPlayers] = useState<Player[]>([]);
+  const [isRoomOwner, setIsRoomOwner] = useState<boolean>(false);
 
-  // Modal states
-  const [opened, setOpened] = useState(false);
-  const form = useForm({
-    initialValues: {
-      username: "",
-    },
-
-    // validate: {
-    //   email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-    // },
-  });
-
-  // Timer
-  const [timer, setTimer] = useState(0);
-  const [isTimerPaused, setIsTimerPaused] = useState(false);
-
-  if (timer < 0) {
-    socket.emit("stop_timer");
-  }
-
-  // Socket responses
-  const pauseTimer = () => {
-    socket.emit("pause_timer");
-  };
-  const resumeTimer = () => {
-    socket.emit("resume_timer");
-  };
-  const restartTimer = () => {
-    socket.emit("restart_timer");
-  };
-  const stopTimer = () => {
-    if (isRoomOwner) {
-      socket.emit("stop_timer");
-    } else {
-      alert("You don't have permission to do that!");
-    }
-  };
-
+  // Server responses
   useEffect(() => {
     socket.on("create-room-msg", (message: string) => {
-      setRoom(getRoomIdFromString(message));
+      setRoomName(getRoomIdFromString(message));
       setIsInRoom(true);
       setIsRoomOwner(true);
     });
 
     socket.on("join-room-msg", (message: string) => {
-      setRoom(getRoomIdFromString(message));
+      setRoomName(getRoomIdFromString(message));
       setIsInRoom(true);
-    });
-
-    socket.on("timer-countdown", (timer: number) => {
-      setTimer(timer);
-    });
-
-    socket.on("is-timer-paused", (isPaused: boolean) => {
-      setIsTimerPaused(isPaused);
     });
 
     socket.on("players-in-room", (players: Player[]) => {
@@ -91,47 +46,7 @@ export default function Home() {
   });
 
   return isInRoom ? (
-    <div>
-      <h1>You have joined room: {room}</h1>
-      <div> {timer.toPrecision(4)} </div>
-      {isTimerPaused && <button onClick={resumeTimer}> Resume timer</button>}
-      {!isTimerPaused && <button onClick={pauseTimer}> Pause timer</button>}
-      <button onClick={restartTimer}> Restart timer</button>
-      <button onClick={stopTimer}> Stop timer</button>
-      {players.map((player: Player) => {
-        return socket.id === player.playerSocketId ? (
-          <div>
-            <Modal
-              opened={opened}
-              onClose={() => setOpened(false)}
-              title="Introduce yourself!"
-            >
-              <form
-                onSubmit={form.onSubmit((values) => {
-                  socket.emit("set_username", values.username);
-                  setOpened(false);
-                })}
-              >
-                <TextInput
-                  withAsterisk
-                  label="Enter your username!"
-                  placeholder="Username"
-                  data-autoFocus
-                  {...form.getInputProps("username")}
-                />
-                <Group position="right" mt="md">
-                  <Button type="submit">Submit</Button>
-                </Group>
-              </form>
-            </Modal>
-            <Button onClick={() => setOpened(true)}>Open Modal</Button>
-            <h2 key="playerSocketId"> {player.username} </h2>
-          </div>
-        ) : (
-          <div key="playerSocketId"> {player.username} </div>
-        );
-      })}
-    </div>
+    <Lobby roomName={roomName} isRoomOwner={isRoomOwner} players={players} />
   ) : (
     <LandingPage />
   );
